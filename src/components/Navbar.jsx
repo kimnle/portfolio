c234f6700260327ebec5c6b87d82f781e1fd7582
kimnle <kimnle002@gmail.com>
2025-01-13 20:05:19 +1100

    styled hamburger icon larger and aligned menu links with logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -21,19 +21,19 @@ const Navbar = () => {
                     onClick={toggleMenu}
                     className="block lg:hidden text-neutral-800 focus:outline-none"
                 >
-                    <IoIosMenu className="w-6 h-6" />
+                    <IoIosMenu className="w-8 h-8" />
                 </button>
 
                 {/* Menu */}
                 <ul
-                    className={`lg:flex space-y-4 lg:space-y-0 lg:space-x-6 absolute lg:static left-0 top-16 w-full lg:w-auto bg-white lg:bg-transparent shadow-lg lg:shadow-none z-10 px-6 py-6 lg:py-0 transition-all duration-300 ease-in-out ${
+                    className={`lg:flex flex-col lg:flex-row space-y-4 lg:space-y-0 lg:space-x-6 fixed lg:static left-0 top-[80px] w-full lg:w-auto bg-white lg:bg-transparent shadow-lg lg:shadow-none z-10 px-6 py-4 pl-14 lg:py-0 lg:pl-0 transition-all duration-300 ease-in-out transform ${
                         isMenuOpen ? "block" : "hidden"
                     }`}
                 >
                     <li>
                         <a
                             href="#about"
-                            className="text-neutral-600 hover:text-yellow-500 font-medium"
+                            className="text-left text-neutral-600 hover:text-yellow-500 font-medium block"
                         >
                             About
                         </a>
@@ -41,7 +41,7 @@ const Navbar = () => {
                     <li>
                         <a
                             href="#projects"
-                            className="text-neutral-600 hover:text-yellow-500 font-medium"
+                            className="text-left text-neutral-600 hover:text-yellow-500 font-medium block"
                         >
                             Projects
                         </a>
