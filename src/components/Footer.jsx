9a18f8348724392e64e10c481352363be03299cf
kimnle <kimnle002@gmail.com>
2025-01-14 16:07:44 +1100

    changed text colour

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
         <footer className="bg-white py-6 text-center">
             <div className="container mx-auto">
                 {/* Footer text */}
-                <p className="text-sm text-neutral-500">
+                <p className="text-sm text-yellow-950">
                     Contacts
                 </p>
                 
@@ -16,7 +16,7 @@ const Footer = () => {
                 <div className="flex justify-center space-x-6 mt-4">
                     <a
                         href="https://www.linkedin.com/in/kim-le-0558bb191/"
-                        className="text-neutral-600 hover:text-yellow-500 transition-colors duration-200"
+                        className="text-stone-800 hover:text-yellow-500 transition-colors duration-200"
                         aria-label="LinkedIn"
                         target="_blank"
                         rel="noopener noreferrer"
@@ -25,14 +25,14 @@ const Footer = () => {
                     </a>
                     <a
                         href="mailto:kimnle002@gmail.com"
-                        className="text-neutral-600 hover:text-yellow-500 transition-colors duration-200"
+                        className="text-stone-800 hover:text-yellow-500 transition-colors duration-200"
                         aria-label="Email"
                     >
                         <MdEmail className="text-3xl" />
                     </a>
                     <a
                         href="https://github.com/kimnle"
-                        className="text-neutral-600 hover:text-yellow-500 transition-clolrs duration-200"
+                        className="text-stone-800 hover:text-yellow-500 transition-colors duration-200"
                         aria-label="GitHub"
                         target="_blank"
                         rel="noopener noreferrer"
