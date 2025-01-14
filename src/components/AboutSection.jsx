a7fb5c5772118d9a3a6f032902c106dba4f4902e
kimnle <kimnle002@gmail.com>
2025-01-14 19:38:11 +1100

    added about section component

---
+++ src/components/AboutSection.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Button from "./Button";
+
+const AboutSection = () => {
+    return (
+        <section className="py-16 bg-neutral-50 text-yellow-950" id="about">
+            
+        </section>
+    )
+}
