d76ce8a9b21a8125b56c01278b8c03bb66c3e470
kimnle <kimnle002@gmail.com>
2025-01-14 19:38:53 +1100

    added conditional link

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const Button = ({ text, onClick, type = "button", size = "md" }) => {
+const Button = ({ text, onClick, type = "button", size = "md", href }) => {
     const style = 
         "inline-block bg-yellow-500 text-white font-medium rounded-full shadow-md transition-all duration-200 focus:outline-none focus:ring-2 focus:ring-yellow-300 focus:ring-offset-2 hover:bg-yellow-600";
     
@@ -11,7 +11,17 @@ const Button = ({ text, onClick, type = "button", size = "md" }) => {
         lg: "px-8 py-4 text-lg",
     };
 
-    return (
+    // Render a button or a link
+    return href ? (
+        <a
+            href={href}
+            target="_blank"
+            rel="noopener noreferrer"
+            className={`${style} ${sizes[size]}`}
+        >
+            {text}
+        </a>
+    ) : (
         <button
             type={type}
             onClick={onClick}
