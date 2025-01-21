236663d98be6c8eb702eac3c81ef6c06ccf1158a
kimnle <kimnle002@gmail.com>
2025-01-21 21:06:01 +1100

    added project 1 and 2

--- src/components/ProjectsSection.jsx
+++ src/components/ProjectsSection.jsx
@@ -1,11 +1,19 @@
 import React from "react";
+import project1 from "../assets/project1.png";
 
 const ProjectsSection = () => {
     const projects = [
         {
             id: 1,
-            name: "Plan and Develop a Potfolio Website",
-            description: 
+            name: "Potfolio",
+            description: "Created a portfolio website which presents me as a dev and IT professional. The purpose was to provide an online portfolio that provides information about me, my skills, interests, professional knowledge and a showcase of my work.",
+            image: "../assets/project1.png",
+        },
+        {
+            id: 2,
+            name: "Terminal Application",
+            description: "Designed, implemented and tested a terminal application and throughout the process demonstrated that I am able to use a range of developer tools.",
+            image: ""
         }
     ]
 }
