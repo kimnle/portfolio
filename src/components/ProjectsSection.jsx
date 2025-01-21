d9fd4bc5fbf88b7fbbf5f003a1e13a9056d10a3b
kimnle <kimnle002@gmail.com>
2025-01-21 12:49:00 +1100

    added projects section component

---
+++ src/components/ProjectsSection.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ProjectsSection = () => {
+    const projects = [
+        {
+            id: 1,
+            name: "Plan and Develop a Potfolio Website",
+            description: 
+        }
+    ]
+}
