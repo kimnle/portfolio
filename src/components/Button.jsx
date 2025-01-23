3ab24c49697db872ce6c7333012cadcbda672ef4
kimnle <kimnle002@gmail.com>
2025-01-23 17:46:01 +1100

    changed border colour

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Button = ({ text, onClick, type = "button", size = "md", href }) => {
     const style = 
-        "inline-block bg-stone-50 text-stone-800 font-medium rounded-full border-solid border border-yellow-700 transition-all duration-200 focus:outline-none focus:ring-2 focus:ring-yellow-200 focus:ring-offset-2 hover:bg-yellow-200";
+        "inline-block bg-stone-50 text-stone-800 font-medium rounded-full border-solid border border-yellow-800 transition-all duration-200 focus:outline-none focus:ring-2 focus:ring-yellow-200 focus:ring-offset-2 hover:bg-yellow-200";
     
     // Size specific styles
     const sizes = {
