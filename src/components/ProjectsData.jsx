21572049bc6872e15aa6e839ef1940c9cf51db62
kimnle <kimnle002@gmail.com>
2025-01-23 17:47:25 +1100

    moved projects array to projectsdata.jsx and added h2 bg

---
+++ src/components/ProjectsData.jsx
@@ -0,0 +1,32 @@
+const projects = [
+    {
+        id: 1,
+        name: "Portfolio",
+        description: "Created a portfolio website which presents me as a dev and IT professional. The purpose was to provide an online portfolio that provides information about me, my skills, interests, professional knowledge and a showcase of my work.",
+        image: "src/assets/project1.png",
+        link: "",
+    },
+    {
+        id: 2,
+        name: "Terminal Application",
+        description: "Designed, implemented and tested a terminal application and throughout the process demonstrated that I am able to use a range of developer tools.",
+        image: "src/assets/project2.png",
+        link: "",
+    },
+    {
+        id: 3,
+        name: "API Webserver",
+        description: "In order to demonstrate my understanding of fundamental programming concepts and database design, I was required to develop an API that works with a database, implementing functionality suitable for a realistic API concept.",
+        image: "src/assets/project3.png",
+        link: ""
+    },
+    {
+        id: 4,
+        name: "Full Stack App",
+        description: "I was to design a web application (app) built for a real world customer, or design a web application (app) built for a hypothetical customer with a realistic problem.",
+        image: "src/assets/project4.png",
+        link: "",
+    }
+]
+
+export default projects;
